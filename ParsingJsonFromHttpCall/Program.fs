01a4b9968ffbc5fce93598a1360bdea3e47d8bf1
Julien Blanchard <julien@sideburns.eu>
2017-06-11 18:23:45 +0200

    Parsing JSON from an HTTP call

---
+++ ParsingJsonFromHttpCall/Program.fs
@@ -0,0 +1,36 @@
+﻿open System
+open System.Net.Http
+open System.Net.Http.Headers
+open System.Threading.Tasks
+open Newtonsoft.Json
+
+type Issue = {
+    Url: string
+    Title: string
+}
+
+let getAsync (url:string) =
+    async {
+        use httpClient = new System.Net.Http.HttpClient()
+        httpClient.DefaultRequestHeaders.Accept.Add(
+                MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"))
+        httpClient.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter")
+
+        use! response = httpClient.GetAsync(url) |> Async.AwaitTask
+        printfn "%A" <| response.Headers.GetValues "Link"
+        response.EnsureSuccessStatusCode () |> ignore
+        let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        return content
+    }
+
+[<EntryPoint>]
+let main argv =
+    let issues =
+        "https://api.github.com/repos/julienXX/terminal-notifier/issues?state=closed&per_page=100&page=1"
+        |> getAsync
+        |> Async.RunSynchronously
+    let deserializedIssues = JsonConvert.DeserializeObject<Issue list>(issues)
+
+    deserializedIssues |> List.iter (fun x -> printfn "Url: %s, Title: %s" x.Url x.Title)
+
+    0 // return an integer exit code
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# GettingStartedWithF-AndDotNetCore
+# Getting Started With F# And .NET Core
+
+In the process of learning F#, I found that examples targetting F# and .NET Core were quite difficult to find.
+I created this repo to document how I did some basic tasks.
+
+## Implemented Tasks
+
+- [x] ParsingJsonFromHttpCall: Making an HTTP call to an API and parsing the JSON response
+- [ ] UsingPostgres: Storing and retrieving data from PostgreSQL
+- [ ] UsingRabbitMQ: Sending and consuming messages to and from RabbitMQ
+- [ ] UsingRedis: Storing and retrieving data from Redis
+- [ ] ?
+
+## Running a task
+
+```
+λ cd ParsingJsonFromHttpCall
+λ dotnet restore
+λ dotnet run
+```
